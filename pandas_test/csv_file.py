a7d043fc3bc3da1ed8db8bcff8a76dd439957a89
Oak-aim <oskyaw.aim.kenshu.26@gmail.com>
2026-06-09 14:02:49 +0900

    updated

--- pandas_test/csv_file.py
+++ pandas_test/csv_file.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 
+# pandasを使用してCSVファイルを読み込む
 df = pd.read_csv('sample.csv')
 print(df)
 
@@ -10,14 +11,19 @@ data = {
     "City": ["New York", "Los Angeles"]
 }
 
+# pandasを使用してCSVファイルを書き込む
 df = pd.DataFrame(data)
 df.to_csv('sample.csv', index=False)
 
+# ----------------------------------------
+
+# csvモジュールを使用してCSVファイルを読み込む
 with open('data.csv', mode='r', encoding='utf-8') as file:    
     reader = csv.reader(file)
     for row in reader:
         print(row)
 
+# csvモジュールを使用してCSVファイルを書き込む
 with open('data.csv', mode='w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Name", "Age", "City"])
